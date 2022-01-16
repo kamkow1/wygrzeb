4c3151d4a117d43e5700339a31eb9f13a1bdbd4a
kamkow1 <ikamileq@gmail.com>
2022-01-15 16:34:36 -0800

    fetching articles to json string

---
+++ Models/Search.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace wygrzebapi.Models
+{
+    public class Search
+    {
+        public int Id { get; set; }
+
+        public string Query { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wygrzebapi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public string Bio { get; set; }
+
+        public string RemoteIpAdress { get; set; }
+    }
+}
